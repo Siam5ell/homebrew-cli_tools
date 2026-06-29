679ba866e4c6c088430e91572fe7a5191091a1a2
siam5ell <jishnusingh1304@gmail.com>
2026-06-29 21:13:28 +0530

    pomo_cli v0.0.3

--- Formula/pomo_cli.rb
+++ Formula/pomo_cli.rb
@@ -2,8 +2,8 @@ class PomoCli < Formula
   desc "A basic CLI tool for pomodoro timer made in C++ with ncurses"
   homepage "https://github.com/siam5ell/pomo_cli"
 
-  url "https://github.com/siam5ell/pomo_cli/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/siam5ell/pomo_cli/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "25261fb161abea8e268d126c2bb52130ebb63f0bb9f2065f6c93f006ccee546a"
 
   license "MIT"
 
