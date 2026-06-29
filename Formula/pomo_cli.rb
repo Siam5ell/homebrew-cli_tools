aaaf6e46e12ccb6f9e20996d9942c596e44b2fbf
siam5ell <jishnusingh1304@gmail.com>
2026-06-29 20:13:08 +0530

    Adding tap for pomo_cli tool

---
+++ Formula/pomo_cli.rb
@@ -0,0 +1,24 @@
+class Pomo_cli < Formula
+  desc "A basic CLI tool for pomodoro timer made in C++ with ncurses"
+  homepage "https://github.com/jishnu/pomo_cli"
+
+  url "https://github.com/jishnu/pomo_cli/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+
+  license "MIT"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                    *std_cmake_args
+
+    system "cmake", "--build", "build"
+
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "#{bin}/yourtool"
+  end
+end
